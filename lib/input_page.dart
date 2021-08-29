0341702e24e82bbccda16f04f71870ff9cf5063f
itayxsagiv <itayxsagiv@gmail.com>
2021-08-29 16:41:31 +0300

    Step 3 - Refactor widgets

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -12,8 +12,54 @@ class _InputPageState extends State<InputPage> {
       appBar: AppBar(
         title: Text('BMI CALCULATOR'),
       ),
-      body: Center(
-        child: Text('Body Text'),
+      body: Column(
+        // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: ReusableCard(cardColor: Color(0xff1d1e33)),
+                ),
+                Expanded(
+                  child: ReusableCard(cardColor: Color(0xff1d1e33)),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: ReusableCard(cardColor: Color(0xff1d1e33)),
+          ),
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: ReusableCard(cardColor: Color(0xff1d1e33)),
+                ),
+                Expanded(
+                  child: ReusableCard(cardColor: Color(0xff1d1e33)),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  final Color cardColor;
+
+  ReusableCard({@required this.cardColor});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15),
+      decoration: BoxDecoration(
+        color: cardColor,
+        borderRadius: BorderRadius.circular(10),
       ),
     );
   }
